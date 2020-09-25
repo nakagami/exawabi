b278c5c363e1ca7e4b9adc55ba2cb49332a0156a
Hajime Nakagami <nakagami@gmail.com>
2020-09-25 17:21:56 +0900

    define nif

--- native/awabi_nif/src/lib.rs
+++ native/awabi_nif/src/lib.rs
@@ -1,6 +1,45 @@
-#[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
+/*
+*MIT License
+*
+*Copyright (c) 2020 Hajime Nakagami
+*
+*Permission is hereby granted, free of charge, to any person obtaining a copy
+*of this software and associated documentation files (the "Software"), to deal
+*in the Software without restriction, including without limitation the rights
+*to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*copies of the Software, and to permit persons to whom the Software is
+*furnished to do so, subject to the following conditions:
+*
+*The above copyright notice and this permission notice shall be included in all
+*copies or substantial portions of the Software.
+*
+*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*SOFTWARE.
+*/
+use awabi::tokenizer;
+
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+  static ref TOK: tokenizer::Tokenizer = tokenizer::Tokenizer::new(None).unwrap();
 }
 
-rustler::init!("Elixir.AwabiNif", [add]);
+#[rustler::nif]
+fn tokenize(s: &str) -> Vec<(String, String)> {
+    TOK.tokenize(s)
+}
+
+#[rustler::nif]
+fn tokenize_n_best(s: &str, n: u32) -> Vec<Vec<(String, String)>> {
+    TOK.tokenize_n_best(s, n)
+}
+
+
+rustler::init!("Elixir.AwabiNif", [tokenize, tokenize_n_best]);
